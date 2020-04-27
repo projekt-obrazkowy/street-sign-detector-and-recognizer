4a3e14b719f3319a2ea97bec9f73dffa3f969adb
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-27 09:15:21 +0200

    main: load opencv library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import org.opencv.core.Core;
+
 public class Main {
     public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println("Hello there");
     }
 }
