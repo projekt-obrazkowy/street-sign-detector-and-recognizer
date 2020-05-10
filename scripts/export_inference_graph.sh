1740e8c402e49e4c0c09263b536b1660a74f425e
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-10 22:05:16 +0200

    scripts: remove

--- scripts/export_inference_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH="$PYTHONPATH:$PWD/imports/slim:$PWD/imports/object_detection"
-
-python3 imports/object_detection/export_inference_graph.py \
-    --input_type image_tensor \
-    --pipeline_config_path configs/ssd_inception_v2_coco.config \
-    --trained_checkpoint_prefix models/train/model.ckpt-425 \
-    --output_directory models/post-train
